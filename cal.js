c65b57d4deb9b8862f3a26d5ed0c509ef3e3636e
Imvishu3012 <vishalprajapati445566@gmail.com>
2025-04-01 14:48:20 +0530

    update the js file

---
+++ cal.js
@@ -0,0 +1,14 @@
+function clr(){
+    document.getElementById("result").value ="";
+
+}
+function display(val){
+    document.getElementById("result").value+=val;
+
+}
+function equate(){
+    var x = document.getElementById('result').value;
+    var y = eval(x);
+    document.getElementById('result').value = y;
+
+}
